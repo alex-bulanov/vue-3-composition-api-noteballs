a92d488c7c112cde21879771d1c2cd690f5d9c30
alex-bulanov <alex-bulanov.mail@yandex.ru>
2024-05-08 20:30:43 +0300

    Установка Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <RouterLink to="/">Заметки</RouterLink>
-  <RouterLink to="/stats">Статистика</RouterLink>
+  <RouterLink class="button" to="/">Заметки</RouterLink>
+  <RouterLink class="button" to="/stats">Статистика</RouterLink>
   <RouterView />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import '../node_modules/bulma/css/bulma.min.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
